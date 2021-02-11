ffc177fa44fea472cfe432b29aa81593f8ae5bd3
bsifue <78883045+bsifue@users.noreply.github.com>
2021-02-11 17:03:23 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,8 @@
 node default {
-  file{'/root/README':
-    ensure    => file,
-    content   => 'This is a readme',
-    owner     => 'root',
-  }
-  
-  file{'/root/helloWorld':
-    ensure    => file,
-    content   => 'hello world - special notes - 
-      All Rescource Types: https://docs.puppet.com/latest/type.html',
-    owner     => 'root',
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
   }
 }
 node 'master.puppet.vm' {
