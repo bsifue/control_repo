6836317c7dd958f2a918974eb2a98dcda4f9faa5
bsifue <78883045+bsifue@users.noreply.github.com>
2021-02-14 15:12:51 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,24 +1,25 @@
 class minecraft {
-  file {'opt/minecraft':
+  file {‘/opt/minecraft’:
     ensure => directory,
   }
-  file {'opt/minecraft/server.jar':
+  file {‘/opt/minecraft/server.jar’:
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
-  package {'java' :
+  package {‘java’:
     ensure => present,
   }
-  file {'opt/minecraft/eula.txt':
+  file {‘/opt/minecraft/eula.txt’:
     ensure => file,
-    content => 'eula-true',
-  } 
-  file {'/etc/systemd/system/minecraft.service':
+    content => ‘eula=true’
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
-    }
-  service {'minecraft' : 
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
     ensure => running,
     enable => true,
-    }
+  }
 }
+
