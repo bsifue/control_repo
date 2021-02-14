27b301838d9c20aa25335866436e346120c09e00
bsifue <78883045+bsifue@users.noreply.github.com>
2021-02-14 15:33:52 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
-class minecraft {
+class minecraft (
   $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   $instal_dir = '/opt/minecraft'
+){
   file {$instal_dir:
     ensure => directory,
   }
