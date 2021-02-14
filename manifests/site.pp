0036f7f6d31210744b6ee93da0859c29b5b028d6
bsifue <78883045+bsifue@users.noreply.github.com>
2021-02-14 15:14:53 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,26 +1,24 @@
 node default {
-    file{'/root/helloWorld':
-    ensure    => file,
-    content   => 'hello world - special notes - 
-    All Rescource Types: https://docs.puppet.com/latest/type.html',
-    owner     => 'root',
-    }
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
-
 node 'master.puppet.vm' {
   include role::master_server
-  
-  file{'/root/README':
-    ensure    => file,
-    content   => "Welcome to marwin $fqdn",
-    }
+  file {'/root/README':
+    ensure => file,
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
+  }
 }
 node 'minetest.puppet.vm': {
   include role::minecraft_server
 }
-node /^web/ {
-    include role::app_server
+node /^web/ { 
+  include role::app_server
 }
 node /^db/ {
-    include role::db_server
+  include role::db_server
 }
