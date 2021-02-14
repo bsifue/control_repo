11812b7af69d2e52cc93689d9199d15949082bf3
bsifue <78883045+bsifue@users.noreply.github.com>
2021-02-14 15:33:15 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,10 +1,12 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  $instal_dir = '/opt/minecraft'
+  file {$instal_dir:
     ensure => directory,
   }
-  file {'/opt/minecraft/server.jar':
+  file {"{$instal_dir}/server.jar":
     ensure      => file,
-    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source      => $url,
     before      => Service['minecraft'],
   }
   package {'java':
@@ -21,7 +23,7 @@ class minecraft {
   service { 'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
+    require => [Package['java'],File["{$instal_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
 
